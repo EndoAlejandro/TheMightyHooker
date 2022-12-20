c33c299572c9be6c816c30137c5e51c53c1f9c46
Alejandro Endo <alejandro.endo@alternova.co>
2022-12-20 23:11:48 +0100

    level 1-1 1-2 n 1-3 updated

--- Assets/Scripts/Hazards/Spikes.cs
+++ Assets/Scripts/Hazards/Spikes.cs
@@ -1,3 +1,4 @@
+using System;
 using PlayerComponents;
 using UnityEngine;
 
@@ -6,13 +7,12 @@ namespace Hazards
     public class Spikes : MonoBehaviour
     {
         public virtual bool IsActive { get; protected set; }
-        
+        private void Awake() => IsActive = true;
         protected virtual void OnTriggerEnter2D(Collider2D col)
         {
             if (!col.TryGetComponent(out Player player)) return;
             KillPLayer(player);
         }
-
         protected void KillPLayer(Player player) => player.Die();
     }
 }
